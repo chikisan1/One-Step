bbbf6b5def85e782be6fab178c7bf9bcea5c1bcf
Chikao Maeda <chikaomaeda@Chikaos-MacBook-Pro.local>
2018-06-26 12:45:53 -0400

    testing swipe

--- One Step/ViewController.swift
+++ One Step/ViewController.swift
@@ -10,14 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgImage.isUserInteractionEnabled = true
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        imgImage.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        imgImage.addGestureRecognizer(swipeLeft)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.up
+        imgImage.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.down
+        imgImage.addGestureRecognizer(swipeDown)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func swipeGesture(sender: UISwipeGestureRecognizer){
+        if let swipeGesture = sender as? UISwipeGestureRecognizer{
+            switch swipeGesture.direction{
+            case UISwipeGestureRecognizerDirection.right:
+                print("Right")
+                imgImage.image = UIImage(named: "2")
+            case UISwipeGestureRecognizerDirection.left:
+                print("Left")
+                imgImage.image = UIImage(named: "3")
+            case UISwipeGestureRecognizerDirection.up:
+                print("Up")
+                imgImage.image = UIImage(named: "4")
+            case UISwipeGestureRecognizerDirection.down:
+                print("Down")
+                imgImage.image = UIImage(named: "1")
+            default:
+                break
+            }
+        }
     }
 
 
