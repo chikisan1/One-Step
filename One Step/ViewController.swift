72d9edbb02af3437fb3a02fcca652135ddd442a8
Chikao Maeda <chikaomaeda@chikaos-mbp.home>
2018-06-25 21:03:00 -0400

    Initial Commit

---
+++ One Step/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  One Step
+//
+//  Created by Chikao Maeda on 6/25/18.
+//  Copyright © 2018 Chikao Maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
